6dff2beea8c05944e54b01f9062fc002daad261a
Melanis Falco <mfalco@uninorte.edu.co>
2017-11-15 19:45:03 +0000

    finished project list

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -10,25 +10,28 @@ import Avatar from 'material-ui/Avatar';
 import Chip from 'material-ui/Chip';
 import FontIcon from 'material-ui/FontIcon';
 import SvgIconFace from 'material-ui/svg-icons/action/face';
+import SvgIconFlag from 'material-ui/svg-icons/content/flag';
 import {blue300, indigo900} from 'material-ui/styles/colors';
 
 
 const styles = {
   block: {
-    maxWidth: 250,
+    maxWidth: 200,
   },
   checkbox: {
-    marginBottom: 16,width: 50, display: 'inline-block',
+    marginBottom: 16,width: 50,position: "absolute",right:0,
   },
    aligning: {
      display: 'inline-block',
   },chip: {
-    margin: 4,
+    margin: 2,
   },
   wrapper: {
     display: 'flex',
     flexWrap: 'wrap',
+    margin:4,
   },
+  
 };
  function handleRequestDelete() {
     alert('You clicked the delete button.');
@@ -47,63 +50,192 @@ class ProjectList extends Component{
         return (
             
             <MuiThemeProvider>
-                <Card>
+            
+     
+                <Card style = {styles.card}>
                     <CardHeader
                               title="Proyecto #1 Mi proyecto"
                               subtitle="fecha de creacion : Hoy ..."
                     />
-                    <p className="descriptionP"   align="justify">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                    </p>
-                    <CardActions>
-                      <FlatButton style={styles.aligning}label="Action1" />
-                      <FlatButton style={styles.aligning}label="Action2" />
+                    <CardText>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    </CardText>
+                 
+                    <CardActions className="visibility">
                        <Checkbox
+                       className="visibility"
                           checkedIcon={<Visibility />}
                           uncheckedIcon={<VisibilityOff />}
-                        align="right"
                          style={styles.checkbox}
                         />
                     </CardActions>
-                    <div style={styles.wrapper}>
+                <div className="contain" style={styles.wrapper}>
             
                     <Chip style={styles.chip}
-                     onRequestDelete={handleRequestDelete}>
-                    
-                      <Avatar color="#444" icon={<SvgIconFace />} />
+                     backgroundColor={'#BCBCBC'}
+                     >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
                       tag1
                     </Chip>
-                    <Chip style={styles.chip}  onRequestDelete={handleRequestDelete}>
-                      <Avatar color="#444" icon={<SvgIconFace />} />
+                    <Chip 
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip} >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
                       tag2
                     </Chip>
-                    <Chip style={styles.chip}  onRequestDelete={handleRequestDelete}>
-                      <Avatar color="#444" icon={<SvgIconFace />} />
+                    <Chip
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip} >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
                       tag3
                     </Chip>
-                    <Chip style={styles.chip}  onRequestDelete={handleRequestDelete}>
-                      <Avatar color="#444" icon={<SvgIconFace />} />
+                    <Chip 
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip}  >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
                       tag4
                     </Chip>
-                    </div>
+                </div>
                     
                             
                 </Card>
-                 <Card>
+             
+                
+                 <Card style = {styles.card}>
                     <CardHeader
-                              title="Proyecto #2"
-                              subtitle="fecha de creacion : Ayer"
+                              title="Proyecto #2 Mi proyecto"
+                              subtitle="fecha de creacion : Hoy ..."
                     />
-                    <p className="descriptionP"   align="justify">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                    </p>
+                    <CardText>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    </CardText>
+                 
+                    <CardActions className="visibility">
+                       <Checkbox
+                       className="visibility"
+                          checkedIcon={<Visibility />}
+                          uncheckedIcon={<VisibilityOff />}
+                         style={styles.checkbox}
+                        />
+                    </CardActions>
+                <div className="contain" style={styles.wrapper}>
+            
+                    <Chip style={styles.chip}
+                     backgroundColor={'#BCBCBC'}
+                     >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag1
+                    </Chip>
+                    <Chip 
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip} >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag2
+                    </Chip>
+                    <Chip
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip} >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag3
+                    </Chip>
+                    <Chip 
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip}  >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag4
+                    </Chip>
+                </div>
+                    
                             
                 </Card>
-                 <Card>
+                 <Card style = {styles.card}>
                     <CardHeader
-                              title="Proyecto #3"
-                              subtitle="fecha de creacion : La semana pasada"
+                              title="Proyecto #3 Mi proyecto"
+                              subtitle="fecha de creacion : Hoy ..."
                     />
-                    <p className="descriptionP"   align="justify">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                    </p>
+                    <CardText>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    </CardText>
+                 
+                    <CardActions className="visibility">
+                       <Checkbox
+                       className="visibility"
+                          checkedIcon={<Visibility />}
+                          uncheckedIcon={<VisibilityOff />}
+                         style={styles.checkbox}
+                        />
+                    </CardActions>
+                <div className="contain" style={styles.wrapper}>
+            
+                    <Chip style={styles.chip}
+                     backgroundColor={'#BCBCBC'}
+                     >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag1
+                    </Chip>
+                    <Chip 
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip} >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag2
+                    </Chip>
+                    <Chip
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip} >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag3
+                    </Chip>
+                    <Chip 
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip}  >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag4
+                    </Chip>
+                </div>
+                    
+                            
+                </Card>
+                 <Card style = {styles.card}>
+                    <CardHeader
+                              title="Proyecto #4 Mi proyecto"
+                              subtitle="fecha de creacion : Hoy ..."
+                    />
+                    <CardText>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    </CardText>
+                 
+                    <CardActions className="visibility">
+                       <Checkbox
+                       className="visibility"
+                          checkedIcon={<Visibility />}
+                          uncheckedIcon={<VisibilityOff />}
+                         style={styles.checkbox}
+                        />
+                    </CardActions>
+                <div className="contain" style={styles.wrapper}>
+            
+                    <Chip style={styles.chip}
+                     backgroundColor={'#BCBCBC'}
+                     >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag1
+                    </Chip>
+                    <Chip 
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip} >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag2
+                    </Chip>
+                    <Chip
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip} >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag3
+                    </Chip>
+                    <Chip 
+                     backgroundColor={'#BCBCBC'}
+                     style={styles.chip}  >
+                      <Avatar color="#444" icon={<SvgIconFlag />} />
+                      tag4
+                    </Chip>
+                </div>
+                    
                             
                 </Card>
         </MuiThemeProvider>
